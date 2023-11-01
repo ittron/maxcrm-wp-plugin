b01a183f5c89d13dbdb95ef9add1740d2b64cd35
Hery <hery@ittron.co.id>
2023-11-01 22:45:02 +0700

    fix js

--- js/omnigo.js
+++ js/omnigo.js
@@ -1,17 +1,21 @@
 window.omnigoSettings = {
-  locale: omnigo_widget_locale,
-  type: omnigo_widget_type,
-  position: omnigo_widget_position,
-  launcherTitle: omnigo_launcher_text,
+	locale: omnigo_widget_locale,
+	type: omnigo_widget_type,
+	position: omnigo_widget_position,
+	launcherTitle: omnigo_launcher_text,
 };
 
-(function(d,t) {
-  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-  g.async=!0;
-  g.defer=!0;
-  g.src="https://omnigo.id/packs/js/sdk.js";
-  s.parentNode.insertBefore(g,s);
-  g.onload=function(){
-    window.omnigoSDK.run({ websiteToken: omnigo_token, baseUrl: omnigo_url })
-  }
-})(document,"script");
+(function (d, t) {
+	var g = d.createElement(t),
+		s = d.getElementsByTagName(t)[0];
+	g.async = !0;
+	g.defer = !0;
+	g.src = "https://omnigo.id/livechat/js";
+	s.parentNode.insertBefore(g, s);
+	g.onload = function () {
+		window.omnigoSDK.run({
+			websiteToken: omnigo_token,
+			baseUrl: omnigo_url,
+		});
+	};
+})(document, "script");
